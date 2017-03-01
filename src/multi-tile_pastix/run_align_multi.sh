705f387e5fb2f181170890a38523dcbba3def8f5
denisovg <denisovg@hhmi.org>
2017-03-01 10:53:47 -0500

    Removing old versions of the code

--- src/multi-tile_pastix/run_align_multi.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-echo "\nRunning the command: mpirun -np $num_slots $TA_HOME/align_multi $TA_DATA/$mat_file\n"
-time mpirun -np $num_slots $TA_HOME/align_multi $TA_DATA/$mat_file $params_file
-
-

5b32c8c89e380b4be607a4528b75e0ca7945258f
denisovg <denisovg@hhmi.org>
2017-03-17 10:01:19 -0400

    Making a default the parameter settings that were used to successfully process the biggest so far dataset, involving ~3.5M tiles

--- src/multi/align_multi.sh
+++ src/multi/align_multi.sh
@@ -2,7 +2,7 @@
 
 export I_MPI_FABRICS=shm:tcp
 export I_MPI_FALLBACK=disable
-qsub -v num_slots=$1,mat_file=$2,params_file=$3 -A flyTEM -R yes -N ats$1 -pe impi $1 -now n -V $TA_BIN/run_align_multi.sh
+qsub -v num_slots=$1,mat_file=$2,params_file=$3 -A flyTEM -R yes -N ats$1 -pe impi2 $1 -now n -V $TA_BIN/run_align_multi.sh
 
 
 
--- src/multi/align_multi_nosplit.sh
+++ src/multi/align_multi_nosplit.sh
@@ -2,7 +2,7 @@
 
 export I_MPI_FABRICS=shm:tcp
 export I_MPI_FALLBACK=disable
-qsub -v num_slots=$1,mat_file=$2,params_file=$3 -A flyTEM -R yes -N at -pe impi $1 -now n -V $TA_BIN/run_align_multi_nosplit.sh
+qsub -v num_slots=$1,mat_file=$2,params_file=$3 -A flyTEM -R yes -N at -pe impi2 $1 -now n -V $TA_BIN/run_align_multi_nosplit.sh
 
 
 

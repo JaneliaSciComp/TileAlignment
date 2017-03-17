540e825bad49bd8c49bc9f4395f34fa294d34a6a
denisovg <denisovg@hhmi.org>
2017-03-16 20:00:09 -0400

    Removed files that are no longer needed

--- src/multi-tile_pastix/align_tiles_split.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-export I_MPI_FABRICS=shm:ofa
-export I_MPI_FALLBACK=disable
-qsub -v num_slots=$1,mat_file=$2,params_file=$3 -A flyTEM -R yes -N ats -pe impi $1 -now n -V $TA_BIN/run_align_tiles_split.sh
-
-
-
